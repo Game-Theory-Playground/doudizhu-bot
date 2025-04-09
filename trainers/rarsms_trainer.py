644253b8fe246cb59870a7ec558ae592721e772f
schromya <schromya@gmail.com>
2025-04-09 15:59:58 -0500

    feat(train) working on code outline more

--- trainers/rarsms_trainer.py
+++ trainers/rarsms_trainer.py
@@ -12,8 +12,11 @@ class RARSMSBotTrainer(BaseTrainer):
         self.num_episodes = num_episodes
         self.cuda = cuda
         self.training_device = training_device
-        self.gamma = 0.001 # TODO: pass this as hyperparameter
-        
+
+        # Hyperparameters
+        self.gamma = 0.001
+        self.lmda = 0.001 
+
     def train(self):
         # Set CUDA device
         device = torch.device(f"cuda:{self.training_device}" if torch.cuda.is_available() else "cpu")
@@ -29,38 +32,40 @@ class RARSMSBotTrainer(BaseTrainer):
         critic_optimizer = optim.Adam(bot.critic_network.parameters(), lr=self.learning_rate)
         
         
-        # Training loop
+        # Each Game (Episode)
         for episode in range(self.num_episodes):
             # Reset environment
             state, player_id = self.env.reset()
-            
-            # Episode loop
+
+            temporal_difference_errors = []
+
+            # Each round (frame)
             while not self.env.is_over():
                 # Choose action
                 action = bot.act(state)
                 
-                # Take action
-                # next_state, reward, done, _ = self.env.step(action)
-                next_state, next_player_id = self.env.step(action)
-
-                # Give rewards
-                # Environment reward only given at end of game (usually 0)
-                environment_reward = self.env.get_payoffs()[player_id] if self.env.is_over() else 0
-                intrinsic_reward = self._calculate_intrinsic_reward()
-                reward = environment_reward + intrinsic_reward  # TODO: Figure out if need both
-
-                temporal_distance_error = reward + self.gamma * self.bot.predict_state(state) + self.bot.predict_state(next_state)
-
-
+                next_state, _ = self.env.step(action)
                 
-                # Store transition
-                # ...
-                
-                # Update networks
-                # ...
-                
+                reward = self._calculate_intrinsic_reward()
+        
+                temporal_difference_error = reward + self.gamma * self.bot.predict_state(state) - self.bot.predict_state(next_state)
+                temporal_difference_errors.append(temporal_difference_error)
                 state = next_state
-                player_id = next_player_id
+            
+
+            advantage_function = self._calculate_advantage(temporal_difference_errors)
+
+            actor_loss = self._calculate_critic_loss(self, advantage_function)
+            critic_loss = self._calculate_critic_loss(self)
+
+            actor_optimizer.zero_grad()
+            actor_loss.backward()
+            actor_optimizer.step()
+
+            critic_optimizer.zero_grad()
+            critic_loss.backward()
+            critic_optimizer.step()
+            
                 
             # Save model periodically
             if episode % 100 == 0:
@@ -80,4 +85,35 @@ class RARSMSBotTrainer(BaseTrainer):
             """
 
             return 0
+        
+
+        def _calculate_advantage_function(self, temporal_difference_errors):
+            T = len(temporal_difference_errors)
+            advantage_function = []
+            for t in range(T):
+                advtg = 0
+                for i in range(t, T):
+                    advtg += (self.gamma * self.lmda) ** (i-t) * temporal_difference_errors[i]
+                advantage_function.append(advtg)
+
+            return advantage_function         
+                 
+
+        def _calculate_actor_loss(self, advantage_function):
+            """ 
+                PPO Clipped Objective Function:
+                This will also need more params
+            """
+
+            # TODO
+        def _calculate_critic_loss(self):
+            """ 
+                Objective function. Not sure what loss function this is.
+                This will also need more params
+            """
+
+
+
+
+
 
